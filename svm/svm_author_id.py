9d4cfc32ff4b50489ea2d613911df874dacf940b
unknown <malab@outlook.com>
2021-07-20 15:37:30 +0530

    Support Vector Machines Lesson done

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -27,13 +27,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 features_train = features_train[:len(features_train)/100]
 labels_train = labels_train[:len(labels_train)/100]
 from sklearn import svm
-cf = svm.SVC(kernel = 'linear',gamma = 'auto')
+cf = svm.SVC(kernel = 'rbf',gamma = 'auto', C= 10000)
 t0 = time()
 cf.fit(features_train, labels_train)
 print "training time:", round(time() - t0, 3), "s"
 t1 = time()
 pred = cf.predict(features_test)
 print "prediction time:", round(time() - t1, 3), "s"
+l = [0, 0]
+for i in pred:
+    if i == 0:
+        l[0] = l[0] + 1
+    else:
+        l[1] = l[1] + 1
+print l
 print cf.score(features_test, labels_test)
 #########################################################
 
