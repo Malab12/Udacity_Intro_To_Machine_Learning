39164f547064656219010adaee995110bf113fbf
unknown <malab@outlook.com>
2021-07-29 11:17:54 +0530

    Lesson 6 finished

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -16,7 +16,19 @@
 """
 
 import pickle
-
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
-
-
+enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+name_list = enron_data.keys()
+count_sal = 0
+count_mail = 0
+count_NaN = 0
+count_poi = 0
+for name in name_list:
+    if enron_data[name]["poi"] == 1:
+        count_poi = count_poi + 1
+    if enron_data[name]["salary"] != 'NaN' :
+        count_sal = count_sal + 1
+    if enron_data[name]["email_address"] != 'NaN':
+        count_mail = count_mail + 1
+    if enron_data[name]["total_payments"] == 'NaN' and enron_data[name]["poi"] == 1:
+        count_NaN = count_NaN + 1
+print count_sal, count_mail, count_NaN, count_poi
