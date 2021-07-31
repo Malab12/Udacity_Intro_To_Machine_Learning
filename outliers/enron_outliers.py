07c0e25724a7388db39045f15fc7d25d9b5febbc
unknown <malab@outlook.com>
2021-07-31 10:26:50 +0530

    Lesson 8 finished

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -5,15 +5,31 @@ import sys
 import matplotlib.pyplot
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-
+from operator import itemgetter
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+data_dict.pop('TOTAL', 0)
 data = featureFormat(data_dict, features)
 
 
 ### your code below
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
+data = sorted(data, key = itemgetter(1), reverse = True)
+max_bonus = data[0][1]
+print max_bonus
+for person in data_dict:
+    if data_dict[person]['bonus'] != 'NaN' and data_dict[person]['salary'] != 'NaN':
+        if data_dict[person]['bonus'] >= 5000000 and data_dict[person]['salary'] >= 1000000:
+            print person
 
-
+    
+print data_dict['LAY KENNETH L']['bonus']
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-
+from operator import itemgetter
 def outlierCleaner(predictions, ages, net_worths):
     """
         Clean away the 10% of points that have the largest
@@ -10,11 +10,20 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
-    cleaned_data = []
-
+    l = []
+    for pred, age, worth in zip(predictions, ages, net_worths):
+        a = age[0]
+        w = worth[0]
+        p = pred[0]
+        
+        tup = (a, w, pow(p - w, 2))
+        #print tup
+        l.append(tup)
+    #print l
+    l = sorted(l, key = itemgetter(2), reverse = True)
+    cleaned = l[9:]
+    cleaned_data = sorted(cleaned, key = itemgetter(0))
     ### your code goes here
 
     
     return cleaned_data
-
