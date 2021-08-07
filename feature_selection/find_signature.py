f1da9157ce759269a5e8534e8fc297d6708f35d7
unknown <malab@outlook.com>
2021-08-07 09:56:46 +0530

    Lesson 12 Finished

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -19,14 +19,15 @@ authors = pickle.load( open(authors_file, "r") )
 ### remainder go into training)
 ### feature matrices changed to dense representations for compatibility with
 ### classifier functions in versions 0.15.2 and earlier
-from sklearn import cross_validation
-features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
                              stop_words='english')
 features_train = vectorizer.fit_transform(features_train)
 features_test  = vectorizer.transform(features_test).toarray()
+print(vectorizer.get_feature_names()[21323])
 
 
 ### a classic way to overfit is to use a small number
@@ -38,6 +39,12 @@ labels_train   = labels_train[:150]
 
 
 ### your code goes here
-
-
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print(clf.score(features_test, labels_test))
+for feature in clf.feature_importances_:
+    if feature >= 0.2:
+        print feature
+        print (numpy.where(clf.feature_importances_ == feature)[0])
 
